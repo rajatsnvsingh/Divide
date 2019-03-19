ff3a4ad67bea3cbf71f132bcdaa4860376fef3ba
Quinn Bischoff <quinn.qb@gmail.com>
2019-03-18 18:43:06 -0600

    Create proper frontened routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 import {App} from './js/components/containers/App.jsx';
+import {Login} from './js/components/containers/Login.jsx';
 
 const render = () => {
   ReactDOM.render(
     <BrowserRouter>
      <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
      </Switch>
     </BrowserRouter>,
     document.getElementById("root")

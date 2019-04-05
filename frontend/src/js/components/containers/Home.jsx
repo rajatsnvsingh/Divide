7ebe35e3d774ae8e3b524913dd783af9471dbbc8
Raavi Mehta <raavi.mehta@ucalgary.ca>
2019-04-05 15:04:31 -0600

    Clean up Home.jsx and move NavigationHeader

--- frontend/src/js/components/containers/Home.jsx
+++ frontend/src/js/components/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import FormContainer from "./FormContainer.jsx";
-import NavigationHeader from "./NavigationHeader/NavigationHeader.jsx";
 import { MainContentContainer } from "./MainContentContainer/MainContentContainer.jsx";
 import SummaryContainer from "./SummaryContainer/SummaryContainer.jsx"
 
-let viewIndexEnum = Object.freeze({"expenses":1, "payments":2});
+let viewIndexEnum = Object.freeze({ "expenses": 1, "payments": 2 });
 
 class Home extends Component {
   constructor() {
@@ -13,29 +11,13 @@ class Home extends Component {
 
   render() {
     return (
-      // <div className="App">
-      //   <div className="col-md-2">
-      //       <SummaryContainer />
-      //   </div>
-      //   <div className="col-md-10">
-      //     <h1>This is the home page</h1>
-      //     <hr/>
-      //     <p>This form will send info through an authenticated socket:</p>
-      //     <FormContainer/>
-      //     <NavigationHeader />
-      //     <MainContentContainer viewIndex={viewIndexEnum.expenses}/>
-      //   </div>
-      // </div>
       <div className="row">
         <SummaryContainer />
-        <div className="col-md-10">
-          <NavigationHeader />
-          <MainContentContainer viewIndex={viewIndexEnum.expenses}/>
-        </div>
+        <MainContentContainer viewIndex={viewIndexEnum.payments} />
       </div>
     );
   }
 }
 
-export  {Home};
-export {viewIndexEnum};
+export { Home };
+export { viewIndexEnum };

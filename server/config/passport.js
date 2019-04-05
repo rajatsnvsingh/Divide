5a2e63c63d5a8bf4a3cd844711c6746cfebe65b3
Quinn Bischoff <quinn.qb@gmail.com>
2019-04-05 13:58:11 -0600

    fix authentication code indentation

--- server/config/passport.js
+++ server/config/passport.js
@@ -28,38 +28,38 @@ passport.use(
     callbackURL:'/auth/google/callback'
     },
     function(request, accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    // make the code asynchronous
-    // User.findOne won't fire until we have all our data back from Google
-    process.nextTick(function() {
-      // try to find the user based on their google id
-      User.findOne({ 'authentication.google.id' : profile.id }, function(err, user) {
-          if (err)
-              return done(err);
+      // asynchronous verification, for effect...
+      // make the code asynchronous
+      // User.findOne won't fire until we have all our data back from Google
+      process.nextTick(function() {
+        // try to find the user based on their google id
+        User.findOne({ 'authentication.google.id' : profile.id }, function(err, user) {
+            if (err)
+                return done(err);
 
-          if (user) {
-              console.log("found user");
-              // if a user is found, log them in
-              return done(null, user);
-          } else {
-            console.log("creating new user");
-              // if the user isnt in our database, create a new user
-              var newUser= new User();
-              // set all of the relevant information
-              newUser.authentication.google.id    = profile.id;
-              newUser.authentication.google.token = accessToken;
-              newUser.authentication.google.name  = profile.displayName;
-              newUser.authentication.google.email = profile.emails[0].value; // pull the first email
-              newUser.name = profile.displayName;
-              // save the user
-              newUser.save(function(err) {
-                  if (err)
-                      throw err;
-                  return done(null, newUser);
-              });
-          }
+            if (user) {
+                console.log("found user");
+                // if a user is found, log them in
+                return done(null, user);
+            } else {
+              console.log("creating new user");
+                // if the user isnt in our database, create a new user
+                var newUser= new User();
+                // set all of the relevant information
+                newUser.authentication.google.id    = profile.id;
+                newUser.authentication.google.token = accessToken;
+                newUser.authentication.google.name  = profile.displayName;
+                newUser.authentication.google.email = profile.emails[0].value; // pull the first email
+                newUser.name = profile.displayName;
+                // save the user
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+        });
       });
-  });
     }
   )
 );

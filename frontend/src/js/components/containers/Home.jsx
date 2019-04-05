bdbd4ad4718694f27d449cfe7f8c3bf54c2ffe3a
aidanBailey <aidan.bailey@ucalgary.ca>
2019-04-05 15:26:15 -0600

    Merge fix

--- frontend/src/js/components/containers/Home.jsx
+++ frontend/src/js/components/containers/Home.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import NavigationHeader from "./NavigationHeader/NavigationHeader.jsx";
-=======
->>>>>>> 7ebe35e3d774ae8e3b524913dd783af9471dbbc8
 import { MainContentContainer } from "./MainContentContainer/MainContentContainer.jsx";
 import SummaryContainer from "./SummaryContainer/SummaryContainer.jsx"
 
@@ -17,14 +13,7 @@ class Home extends Component {
     return (
       <div className="row">
         <SummaryContainer />
-<<<<<<< HEAD
-        <div className="col-md-10">
-          <NavigationHeader viewIndex={viewIndexEnum.expenses}/>
-          <MainContentContainer viewIndex={viewIndexEnum.expenses}/>
-        </div>
-=======
         <MainContentContainer viewIndex={viewIndexEnum.payments} />
->>>>>>> 7ebe35e3d774ae8e3b524913dd783af9471dbbc8
       </div>
     );
   }

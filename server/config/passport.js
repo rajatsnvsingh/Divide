9082518d71f114114c8cb5db7ae82bcf459bb619
Quinn Bischoff <quinn.qb@gmail.com>
2019-04-05 13:53:41 -0600

    Add authenticated user accounts
    User account is created upon first login.

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,10 @@
 
 var passport = require("passport");
 const keys = require('./authKey');
+const mongoose = require("mongoose");
+
+// load up the user model
+var User = mongoose.model('User');
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //This is our google authentication api
@@ -24,15 +28,38 @@ passport.use(
     callbackURL:'/auth/google/callback'
     },
     function(request, accessToken, refreshToken, profile, done) {
-      // asynchronous verification, for effect...
-      process.nextTick(function () {
-        
-        // To keep the example simple, the user's Google profile is returned to
-        // represent the logged-in user.  In a typical application, you would want
-        // to associate the Google account with a user record in your database,
-        // and return that user instead.
-        return done(null, profile);
+    // asynchronous verification, for effect...
+    // make the code asynchronous
+    // User.findOne won't fire until we have all our data back from Google
+    process.nextTick(function() {
+      // try to find the user based on their google id
+      User.findOne({ 'authentication.google.id' : profile.id }, function(err, user) {
+          if (err)
+              return done(err);
+
+          if (user) {
+              console.log("found user");
+              // if a user is found, log them in
+              return done(null, user);
+          } else {
+            console.log("creating new user");
+              // if the user isnt in our database, create a new user
+              var newUser= new User();
+              // set all of the relevant information
+              newUser.authentication.google.id    = profile.id;
+              newUser.authentication.google.token = accessToken;
+              newUser.authentication.google.name  = profile.displayName;
+              newUser.authentication.google.email = profile.emails[0].value; // pull the first email
+              newUser.name = profile.displayName;
+              // save the user
+              newUser.save(function(err) {
+                  if (err)
+                      throw err;
+                  return done(null, newUser);
+              });
+          }
       });
+  });
     }
   )
 );

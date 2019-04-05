383cc037edb0b7b4ebca5064ccb4cbc90409eb43
Raavi Mehta <raavi.mehta@ucalgary.ca>
2019-04-05 15:53:01 -0600

    Extract ExpenseContainer and PaymentContainer

---
+++ frontend/src/js/components/containers/ExpenseContainer/ExpenseContainer.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ExpenseInputContainer from "../../presentational/ExpenseInput/ExpenseInputContainer.jsx";
+import ExpenseList from "../ExpenseList/ExpenseList.jsx";
+
+class ExpenseContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-10 removePadding">
+                        <ExpenseInputContainer />
+                    </div>
+                    <div className="col-2 removePadding">
+                        <button className="btn-dark newExpenseBtn removePadding text-center">
+                            <span className="additionSymbol">&#43;</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <ExpenseList />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ExpenseContainer;
--- frontend/src/js/components/containers/MainContentContainer/MainContentContainer.jsx
+++ frontend/src/js/components/containers/MainContentContainer/MainContentContainer.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { viewIndexEnum } from "../Home.jsx";
-import ExpenseInputContainer from "../../presentational/ExpenseInput/ExpenseInputContainer.jsx";
-import ExpenseList from "../ExpenseList/ExpenseList.jsx";
-import PaymentList from "../PaymentList/PaymentList.jsx";
+import ExpenseContainer from "../ExpenseContainer/ExpenseContainer.jsx";
+import PaymentContainer from "../PaymentContainer/PaymentContainer.jsx";
 import NavigationHeader from "../NavigationHeader/NavigationHeader.jsx";
 import "./MainContentContainer.css";
 
@@ -16,53 +15,17 @@ class MainContentContainer extends Component {
             return (
                 <div className={this.props.className}>
                     <NavigationHeader />
-                    <div className="row">
-                        <div className="col-10 removePadding">
-                            <ExpenseInputContainer />
-                        </div>
-                        <div className="col-2 removePadding">
-                            <button className="btn-dark newExpenseBtn removePadding text-center">
-                                <span className="additionSymbol">&#43;</span>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <ExpenseList />
-                        </div>
-                    </div>
+                    <ExpenseContainer />
                 </div>
             );
         }
+
         else {
             return (
                 <div className={this.props.className}>
                     <NavigationHeader />
-                    <div className="row mt-3">
-                        <div className="col-md-4">
-                            <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="Search" />
-                                <div className="input-group-append">
-                                    <button className="btn btn-primary" type="button">Search</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <select className="form-control">
-                                <option>Status...</option>
-                            </select>
-                        </div>
-                        <div className="col-md-2">
-                            <select className="form-control">
-                                <option>Sort By...</option>
-                            </select>
-                        </div>
-                        <div className="col">
-                            <button type="button" className="btn btn-primary w-100">Enter a Payment</button>
-                        </div>
-                    </div>
-                    <PaymentList />
-                </div>
+                    <PaymentContainer />
+               </div>
             );
         }
     }

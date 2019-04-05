5875af59c2062ef014708c4fb3b150e67dc0254a
Raavi Mehta <raavi.mehta@ucalgary.ca>
2019-04-05 16:26:05 -0600

    Extract PaymentInputContainer from PaymentContainer

--- frontend/src/js/components/containers/PaymentContainer/PaymentContainer.jsx
+++ frontend/src/js/components/containers/PaymentContainer/PaymentContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PaymentList from "../PaymentList/PaymentList.jsx";
+import PaymentInputContainer from "../PaymentInputContainer/PaymentInputContainer.jsx";
 
 class PaymentContainer extends Component {
     constructor(props) {
@@ -9,29 +10,7 @@ class PaymentContainer extends Component {
     render() {
         return (
             <div>
-                <div className="row mt-3">
-                    <div className="col-md-4">
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Search" />
-                            <div className="input-group-append">
-                                <button className="btn btn-primary" type="button">Search</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-2">
-                        <select className="form-control">
-                            <option>Status...</option>
-                        </select>
-                    </div>
-                    <div className="col-md-2">
-                        <select className="form-control">
-                            <option>Sort By...</option>
-                        </select>
-                    </div>
-                    <div className="col">
-                        <button type="button" className="btn btn-primary w-100">Enter a Payment</button>
-                    </div>
-                </div>
+                <PaymentInputContainer />
                 <PaymentList />
             </div>
         );

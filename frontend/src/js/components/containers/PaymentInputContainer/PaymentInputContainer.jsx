c5ef11a86adfb491adad2c9fb3d15a539bd968b1
raavi-mehta <raavism@gmail.com>
2019-04-11 20:27:37 -0600

    Add callbacks within PaymentInputContainer

--- frontend/src/js/components/containers/PaymentInputContainer/PaymentInputContainer.jsx
+++ frontend/src/js/components/containers/PaymentInputContainer/PaymentInputContainer.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import { FilterEnum, SortEnum } from "../PaymentContainer/PaymentContainer.jsx";
 
 class PaymentInputContainer extends Component {
     constructor(props) {
         super(props);
         this.onNewPaymentClick = this.onNewPaymentClick.bind(this);
+        this.onFilterChange = this.onFilterChange.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
+        this.onSortChange = this.onSortChange.bind(this);
     }
 
     onNewPaymentClick() {
         this.props.onNewPaymentChange(true);
+    }
+
+    onFilterChange(event) {
+        this.props.onFilterChange(event.target.value);
+    }
+
+    onSearchChange(event) {
+        this.props.onSearchChange(event.target.value);
+    }
+
+    onSortChange(event) {
+        this.props.onSortChange(event.target.value);
     }
 
     render() {
@@ -16,20 +32,23 @@ class PaymentInputContainer extends Component {
                 <div className="row mt-3">
                     <div className="col-md-4">
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Search" />
+                            <input type="text" className="form-control" placeholder="Search" onChange={this.onSearchChange} />
                             <div className="input-group-append">
                                 <button className="btn btn-primary" type="button">Search</button>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-2">
-                        <select className="form-control">
-                            <option>Status...</option>
+                        <select className="form-control" onChange={this.onFilterChange}>
+                            <option value={FilterEnum.none}>No Filter</option>
+                            <option value={FilterEnum.completed}>Completed</option>
+                            <option value={FilterEnum.pending}>Pending</option>
                         </select>
                     </div>
                     <div className="col-md-2">
-                        <select className="form-control">
-                            <option>Sort By...</option>
+                        <select className="form-control" onChange={this.onSortChange}>
+                            <option value={SortEnum.date}>Latest</option>
+                            <option value={SortEnum.name}>Name</option>
                         </select>
                     </div>
                     <div className="col">

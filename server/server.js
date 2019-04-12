a81aa6534a8a5032dc6ac4838e552a5827a239b1
Rajat Singh <rajat.singh@ucalgary.ca>
2019-04-12 11:58:03 -0600

    refactor backend and implement socketManager

--- server/server.js
+++ server/server.js
@@ -5,22 +5,22 @@ const logger = require("morgan");
 const passport = require("passport");
 const API_PORT = 3001;
 const app = express();
-var cookieParser = require('cookie-parser');
+var cookieParser = require("cookie-parser");
 app.use(cookieParser());
-var http = require('http').Server(app);
-var passportSocketIo = require('passport.socketio');
-var io = require('socket.io')(http);
-io.origins('*:*');
+var http = require("http").Server(app);
+var passportSocketIo = require("passport.socketio");
+var io = require("socket.io")(http);
+io.origins("*:*");
 const router = express.Router();
 const mongoose = require("mongoose");
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 const fs = require("fs");
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const path = require("path");
 
 // Database Configuration ======================================================
-var {db} = require("./config/database");
+var { db } = require("./config/database");
 
 fs.readdirSync(__dirname + "/app/models").forEach(function(filename) {
   if (~filename.indexOf(".model.js"))
@@ -31,7 +31,6 @@ fs.readdirSync(__dirname + "/app/routes").forEach(function(filename) {
   if (~filename.indexOf(".route.js"))
     require(__dirname + "/app/routes/" + filename)(router);
 });
-
 // Other Configuration =========================================================
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //parses request bodies to json format
@@ -41,62 +40,56 @@ app.use(logger("dev")); //for logging
 // Sets up a session store with Mongo
 var sessionStore = new MongoStore({ mongooseConnection: db });
 
-app.use(session({
-  store: sessionStore,
-  secret: 'dividesecretencryptkey',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    store: sessionStore,
+    secret: "dividesecretencryptkey",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 require("./config/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
 // routes ======================================================================
-app.use('/api', router);
+app.use("/api", router);
 // load our routes and pass in our app and fully configured passport
 require("./app/routes.js")(app, router, passport);
 
 //FOR PRODUCTION BUILDS, SET TO TRUE
-if(false) {
+if (false) {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'app/public/build')));
+  app.use(express.static(path.join(__dirname, "app/public/build")));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'app/public/build', 'index.html'));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/build", "index.html"));
   });
 }
 
-
-io.use(passportSocketIo.authorize({
-  key: 'connect.sid',
-  store: sessionStore,
-  secret: 'dividesecretencryptkey',
-  passport: passport,
-  cookieParser: cookieParser,
-  fail: onAuthorizeFail,     // *optional* callback on fail/error
-}));
+io.use(
+  passportSocketIo.authorize({
+    key: "connect.sid",
+    store: sessionStore,
+    secret: "dividesecretencryptkey",
+    passport: passport,
+    cookieParser: cookieParser,
+    fail: onAuthorizeFail // *optional* callback on fail/error
+  })
+);
 
 // launch our backend into a port
 http.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
 
-io.on('connection', function (socket) {
-  console.log("new connection");
-
-  socket.on('test', function (data) {
-    // user data from the socket.io passport middleware
-    if (socket.request.user && socket.request.user.logged_in) {
-      console.log("authenticated message rcvd: " + data.value);
-    }
-    else {
-    console.log("NON-AUTHENTICATED MSG RCVD:" + data.value);
-    }
-  });
+const socketManager = require("./app/socketManager.js");
+io.on("connection", function(socket) {
+  socketManager(socket);
 });
 
-function onAuthorizeFail(data, message, error, accept){
+function onAuthorizeFail(data, message, error, accept) {
   // error indicates whether the fail is due to an error or just a unauthorized client
-  if(error)  throw new Error(message);
+  if (error) throw new Error(message);
   // send the (not-fatal) error-message to the client and deny the connection
   return accept(new Error(message));
 }

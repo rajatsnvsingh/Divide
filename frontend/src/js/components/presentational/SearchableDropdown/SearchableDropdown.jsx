0f48768fbaf24ef70375ca2fdfa5e4b500d4fa53
Raavi Mehta <raavi.mehta@ucalgary.ca>
2019-04-13 10:45:10 -0600

    Fix searching and style for SearchDropDown

--- frontend/src/js/components/presentational/SearchableDropdown/SearchableDropdown.jsx
+++ frontend/src/js/components/presentational/SearchableDropdown/SearchableDropdown.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import '../../presentational/ExpenseInput/ExpenseInputContainer.css';
+import { socket } from "../../../../App.js";
 
 class SearchableDropdown extends Component {
     constructor(props) {
@@ -13,6 +15,21 @@ class SearchableDropdown extends Component {
             searchUsers: [],
             selectedUser: null
         };
+    }
+
+    componentDidMount() {
+        socket.emit(
+            "get_all_users",
+            function (userList) {
+                this.setState({
+                    searchUsers: userList
+                });
+            }.bind(this)
+        );
+    }
+
+    onSearchTextChanged(event) {
+        this.setState({ searchText: event.target.value });
     }
 
     onSearchTextChanged(event) {
@@ -39,7 +56,12 @@ class SearchableDropdown extends Component {
     }
 
     render() {
-        const userOptions = this.state.searchUsers.map(userOption => (
+        // Filter list of users to show in dropdown
+        const searchUsers = this.state.searchUsers.filter(user =>
+            user.name.toLowerCase().startsWith(this.state.searchText.toLowerCase())
+        );
+
+        const userOptions = searchUsers.map(userOption => (
             <a
                 key={userOption._id}
                 id={userOption._id}
@@ -53,26 +75,17 @@ class SearchableDropdown extends Component {
         ));
 
         return (
-            <div className="form-control mr-1">
-                <label className="col-form-label">Split With:</label>
+            <div className="form-control border-0 mr-1">
                 <button
                     type="button"
-                    id="splitbutton"
                     className="btn btn-default dropdown-toggle btn-block text-left sfbtn"
                     type="button"
                     data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
                     onClick={this.onSearchButtonClicked}
                 >
-                    {this.state.selectedUser !== null
-                        ? this.state.selectedUser.name
-                        : "Select User"}
+                    {this.state.selectedUser !== null ? this.state.selectedUser.name : "Select User"}
                 </button>
-                <div
-                    className={"dropdown-menu " + this.state.searchDropdown}
-                    aria-labelledby="dropdownMenuButton"
-                >
+                <div className={this.state.searchDropdown === "show" ? "" : "d-none"}>
                     <input
                         type="text"
                         className="form-control-text ml-1 mr-1"
